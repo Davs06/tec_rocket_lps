ef472e57cc41de75b993481214e67af9d9c853ad
Davs06 <santosdv40@gmail.com>
2026-01-23 20:29:25 -0300

    voltando o processa

--- processa.php
+++ processa.php
@@ -1,5 +1,4 @@
 <?php
-include('index.php');
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -11,18 +10,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         'email' => $_POST['email'],
         'whatsapp' => $_POST['whatsapp'],
         'dominio' => $_SERVER['HTTP_HOST'],
-        'pagina' => $projeto
+        // 'pagina' => $projeto
     ];
 
     // Redireciona direto para o checkout da Hotmart com os dados preenchidos
     // Substitua pelo seu link de checkout real
 
-    if ($_SERVER['HTTP_HOST'] == 'techrocket.site' && $uri == 'produtos/ebook-maternidade') {
-        $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
-    } else {
-        // Outros domínios podem ter URLs de checkout diferentes
-        $checkout_url = "";
-    }
+    // if ($_SERVER['HTTP_HOST'] == 'techrocket.site' && $uri == 'produtos/ebook-maternidade') {
+    //     $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
+    // } else {
+    //     // Outros domínios podem ter URLs de checkout diferentes
+    //     $checkout_url = "";
+    // }
     $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
 
     header("Location: $checkout_url");
