f4117fb076b6ab727d9dc88114d735039956da65
Davs06 <santosdv40@gmail.com>
2026-01-23 20:19:17 -0300

    teste processo

--- processa.php
+++ processa.php
@@ -1,4 +1,7 @@
 <?php
+include('index.php');
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = urlencode($_POST['nome']);
     $email = urlencode($_POST['email']);
@@ -7,11 +10,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         'nome' => $_POST['nome'],
         'email' => $_POST['email'],
         'whatsapp' => $_POST['whatsapp'],
-        'dominio' => $_SERVER['HTTP_HOST']
+        'dominio' => $_SERVER['HTTP_HOST'],
+        'pagina' => $projeto
     ];
 
     // Redireciona direto para o checkout da Hotmart com os dados preenchidos
     // Substitua pelo seu link de checkout real
+
+    if ($_SERVER['HTTP_HOST'] == 'techrocket.site' && $uri == 'produtos/ebook-maternidade') {
+        $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
+    } else {
+        // Outros domínios podem ter URLs de checkout diferentes
+        $checkout_url = "";
+    }
     $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
 
     header("Location: $checkout_url");
