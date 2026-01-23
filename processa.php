26ff8364030c5b0ce998216605336396c0b3c091
Davs06 <santosdv40@gmail.com>
2026-01-23 20:35:54 -0300

    teste de url

--- processa.php
+++ processa.php
@@ -1,28 +1,42 @@
 <?php
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = urlencode($_POST['nome']);
     $email = urlencode($_POST['email']);
+    $whatsapp = $_POST['whatsapp'];
 
+    // Captura o nome do projeto vindo do campo oculto
+    $projeto = $_POST['projeto_origem'] ?? 'desconhecido';
+
+    // Lógica de Direcionamento e Tags
+    switch ($projeto) {
+        case 'maternidade_lucro':
+            $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
+            // Aqui você dispararia a tag para o funil de maternidade
+            break;
+
+        // case 'tech_rocket_institucional':
+        //     // Redireciona para uma página de "Obrigado" ou WhatsApp da Tec Rocket
+        //     $checkout_url = "obrigado.php?projeto=techrocket";
+        //     break;
+
+        // case 'bratz_style':
+        //     $checkout_url = "https://checkout.exemplo.com/bratz?name=$nome";
+        //     break;
+
+        default:
+            $checkout_url = "obrigado.php";
+            break;
+    }
+
+    // Opcional: Logar os dados para o seu sistema de automação (n8n/Webhooks)
     $data = [
         'nome' => $_POST['nome'],
         'email' => $_POST['email'],
-        'whatsapp' => $_POST['whatsapp'],
+        'whatsapp' => $whatsapp,
         'dominio' => $_SERVER['HTTP_HOST'],
-        // 'pagina' => $projeto
+        'projeto' => $projeto // Agora você tem o nome exato do projeto aqui!
     ];
-
-    // Redireciona direto para o checkout da Hotmart com os dados preenchidos
-    // Substitua pelo seu link de checkout real
-
-    // if ($_SERVER['HTTP_HOST'] == 'techrocket.site' && $uri == 'produtos/ebook-maternidade') {
-    //     $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
-    // } else {
-    //     // Outros domínios podem ter URLs de checkout diferentes
-    //     $checkout_url = "";
-    // }
-    $checkout_url = "https://pay.hotmart.com/F104029048E?name=$nome&email=$email";
 
     header("Location: $checkout_url");
     exit();
