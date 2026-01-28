ccd4842ef772b12020271980c64f31e09c91506a
Davs06 <santosdv40@gmail.com>
2026-01-27 22:53:48 -0300

    modified:   temas/karate.php

--- temas/karate.php
+++ temas/karate.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/karate.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/karate.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/assets/css/karate.css?v=<?php echo time(); ?>">
 
     <link rel="icon" type="image/png" href="/assets/img/material m braga/favicon-karate/favicon-96x96.png"
         sizes="96x96" />
